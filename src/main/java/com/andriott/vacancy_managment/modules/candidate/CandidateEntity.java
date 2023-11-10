e3c0e5936378920d1e9255d334c71bf8aa8d4f25
Thiago Andriotti <andriott7@gmail.com>
2023-11-10 10:14:05 -0300

    formatting validation messages

--- src/main/java/com/andriott/vacancy_managment/modules/candidate/CandidateEntity.java
+++ src/main/java/com/andriott/vacancy_managment/modules/candidate/CandidateEntity.java
@@ -5,6 +5,7 @@ import java.util.UUID;
 import org.hibernate.validator.constraints.Length;
 
 import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Pattern;
 import lombok.Data;
 
@@ -14,13 +15,14 @@ public class CandidateEntity {
   private UUID id;
   private String name;
 
-  @Pattern(regexp = "^[^\\\\s]+$", message = "[username]  field cannot contain spaces")
+  @NotBlank(message = "[username] field cannot be blank")
+  @Pattern(regexp = "\\S+", message = "[username]  field cannot contain spaces")
   private String username;
 
   @Email(message = "Please enter a valid [email] address")
   private String email;
 
-  @Length(min = 10, max = 100)
+  @Length(min = 10, max = 100, message = "[password] must contain (10) to (100) characters")
   private String password;
   private String description;
   private String curriculum;
