a2cf4583bc55b04020d845036374e8ecd2ae991c
Thiago Andriotti <andriott7@gmail.com>
2023-11-13 23:52:39 -0300

    usecase file for create company

---
+++ src/main/java/com/andriott/vacancy_managment/modules/company/repositories/CompanyRepository.java
@@ -0,0 +1,11 @@
+package com.andriott.vacancy_managment.modules.company.repositories;
+
+import java.util.Optional;
+import java.util.UUID;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.andriott.vacancy_managment.modules.company.entities.CompanyEntity;
+
+public interface CompanyRepository extends JpaRepository<CompanyEntity, UUID> {
+  Optional<CompanyEntity> findByUsernameOrEmail(String username, String email);
+}
