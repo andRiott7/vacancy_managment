b1c6f85b2988d17de60c4f787f4e64104bf6bb43
Thiago Andriotti <andriott7@gmail.com>
2023-11-16 23:20:56 -0300

    security filter and retrieving jwt

--- src/main/java/com/andriott/vacancy_managment/modules/company/useCases/AuthCompanyUseCase.java
+++ src/main/java/com/andriott/vacancy_managment/modules/company/useCases/AuthCompanyUseCase.java
@@ -1,5 +1,8 @@
 package com.andriott.vacancy_managment.modules.company.useCases;
 
+import java.time.Instant;
+import java.time.Duration;
+
 import javax.naming.AuthenticationException;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -39,7 +42,8 @@ public class AuthCompanyUseCase {
     Algorithm algorithm = Algorithm.HMAC256(secretKey);
 
     var token = JWT.create().withIssuer("javagas")
-        .withSubject(company.getId().toString()).sign(algorithm);
+      .withExpiresAt(Instant.now().plus(Duration.ofHours(2)))
+      .withSubject(company.getId().toString()).sign(algorithm);
 
     return token;
   }
