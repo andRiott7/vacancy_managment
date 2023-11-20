d605b634b22021969e7853e870c428370ec073a4
Thiago Andriotti <andriott7@gmail.com>
2023-11-20 00:23:10 -0300

    refactoring the job controller with dto

---
+++ src/main/java/com/andriott/vacancy_managment/modules/company/dto/CreateJobDTO.java
@@ -0,0 +1,11 @@
+package com.andriott.vacancy_managment.modules.company.dto;
+
+import lombok.Data;
+
+@Data
+public class CreateJobDTO {
+
+  private String description;
+  private String benefits;
+  private String level;
+}
