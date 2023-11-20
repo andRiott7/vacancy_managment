bb8975357d51215581b2c396b1744f1b54ff448a
Thiago Andriotti <andriott7@gmail.com>
2023-11-20 00:11:20 -0300

    refactoring the jobcontroller

--- src/main/java/com/andriott/vacancy_managment/modules/company/controllers/JobController.java
+++ src/main/java/com/andriott/vacancy_managment/modules/company/controllers/JobController.java
@@ -1,5 +1,7 @@
 package com.andriott.vacancy_managment.modules.company.controllers;
 
+import java.util.UUID;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -8,6 +10,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.andriott.vacancy_managment.modules.company.entities.JobEntity;
 import com.andriott.vacancy_managment.modules.company.useCases.CreateJobUseCase;
+import jakarta.servlet.http.HttpServletRequest;
 
 import jakarta.validation.Valid;
 
@@ -19,7 +22,11 @@ public class JobController {
   private CreateJobUseCase createJobUseCase;
 
   @PostMapping("/")
-  public JobEntity create(@Valid @RequestBody JobEntity jobEntity) {
-    return createJobUseCase.execute(jobEntity);
+  public JobEntity create(@Valid @RequestBody JobEntity jobEntity, HttpServletRequest request) {
+    var companyId = request.getAttribute("company_id");
+
+    jobEntity.setCompanyId(UUID.fromString(companyId.toString()));
+
+    return this.createJobUseCase.execute(jobEntity);
   }
 }
