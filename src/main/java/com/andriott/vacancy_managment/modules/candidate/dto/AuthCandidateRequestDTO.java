01a345effb387e7e245f825f78c0b643f55ca2d5
Thiago Andriotti <andriott7@gmail.com>
2023-11-22 23:55:08 -0300

    creating token for the candidate

---
+++ src/main/java/com/andriott/vacancy_managment/modules/candidate/dto/AuthCandidateRequestDTO.java
@@ -0,0 +1,7 @@
+package com.andriott.vacancy_managment.modules.candidate.dto;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+public record AuthCandidateRequestDTO(String username, String password) {
+
+}
---
+++ src/main/java/com/andriott/vacancy_managment/modules/candidate/useCases/AuthCandidateUseCase.java
@@ -0,0 +1,61 @@
+package com.andriott.vacancy_managment.modules.candidate.useCases;
+
+import java.time.Instant;
+import javax.security.sasl.AuthenticationException;
+
+import java.time.Duration;
+import java.time.Instant;
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+import com.andriott.vacancy_managment.modules.candidate.CandidateRepository;
+import com.andriott.vacancy_managment.modules.candidate.dto.AuthCandidateRequestDTO;
+import com.andriott.vacancy_managment.modules.candidate.dto.AuthCandidateResponseDTO;
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.algorithms.Algorithm;
+
+@Service
+public class AuthCandidateUseCase {
+
+  @Value("${security.token.secret.candidate}")
+  private String secretKey;
+
+  @Autowired
+  private CandidateRepository candidateRepository;
+
+  @Autowired
+  private PasswordEncoder passwordEncoder;
+
+  public AuthCandidateResponseDTO execute(AuthCandidateRequestDTO authCandidateRequestDTO)
+      throws AuthenticationException {
+    var candidate = this.candidateRepository.findByUsername(authCandidateRequestDTO.username())
+        .orElseThrow(() -> {
+          throw new UsernameNotFoundException("Username/password incorrect");
+        });
+
+    var passwordMatches = this.passwordEncoder
+        .matches(authCandidateRequestDTO.password(), candidate.getPassword());
+
+    if (!passwordMatches) {
+      throw new AuthenticationException();
+    }
+
+    Algorithm algorithm = Algorithm.HMAC256(secretKey);
+    var token = JWT.create()
+        .withIssuer("javagas")
+        .withSubject(candidate.getId().toString())
+        .withClaim("roles", Arrays.asList("candidate"))
+        .withExpiresAt(Instant.now().plus(Duration.ofMinutes(10)))
+        .sign(algorithm);
+
+    var AuthCandidateResponse = AuthCandidateResponseDTO.builder()
+        .access_token(token).build();
+
+    return AuthCandidateResponse;
+  }
+}
