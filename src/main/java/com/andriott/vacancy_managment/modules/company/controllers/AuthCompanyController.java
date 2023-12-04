df7d3fa15de5851ae3305aca4e51f37997239a68
Thiago Andriotti <andriott7@gmail.com>
2023-12-03 22:56:43 -0300

    candidate security filter part2

--- src/main/java/com/andriott/vacancy_managment/modules/company/controllers/AuthCompanyController.java
+++ src/main/java/com/andriott/vacancy_managment/modules/company/controllers/AuthCompanyController.java
@@ -12,13 +12,13 @@ import com.andriott.vacancy_managment.modules.company.dto.AuthCompanyDTO;
 import com.andriott.vacancy_managment.modules.company.useCases.AuthCompanyUseCase;
 
 @RestController
-@RequestMapping("/auth")
+@RequestMapping("/company")
 public class AuthCompanyController {
 
   @Autowired
   private AuthCompanyUseCase authCompanyUseCase;
 
-  @PostMapping("/company")
+  @PostMapping("/auth")
   public ResponseEntity<Object> create(@RequestBody AuthCompanyDTO authCompanyDTO) {
 
     try {
--- src/main/java/com/andriott/vacancy_managment/security/SecurityCandidateFilter.java
+++ src/main/java/com/andriott/vacancy_managment/security/SecurityCandidateFilter.java
@@ -1,5 +1,47 @@
 package com.andriott.vacancy_managment.security;
 
-public class SecurityCandidateFilter {
+import java.io.IOException;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import com.andriott.vacancy_managment.providers.JWTCandidateProvider;
+
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+@Component
+public class SecurityCandidateFilter extends OncePerRequestFilter {
+
+  @Autowired
+  private JWTCandidateProvider jwtProvider;
+
+  @Override
+  protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
+      throws ServletException, IOException {
+    SecurityContextHolder.getContext().setAuthentication(null);
+    String header = request.getHeader("Authorization");
+
+    if (request.getRequestURI().startsWith("/candidate")) {
+      if (header != null) {
+        var token = this.jwtProvider.validateToken(header);
+
+        if (token == null) {
+          response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+          return;
+        }
+
+        request.setAttribute("candidate_id", token.getSubject());
+        var roles = token.getClaim("roles");
+      }
+
+    }
+
+    filterChain.doFilter(request, response);
+  }
 
 }
